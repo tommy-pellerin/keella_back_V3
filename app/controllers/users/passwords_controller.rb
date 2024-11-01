46a1a87c3dec32ef95237d14f74deb9c78b27b3c
tommy-pellerin <tommytp@hotmail.fr>
2024-11-01 20:22:48 +0100

    can update password

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,21 +1,21 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_action :authenticate_user!
 
-  def update_password
-    user_params = params.require(:user).permit(:current_password, :new_password, :new_password_confirmation)
+  def update
+    user_params = params.require(:user).permit(:current_password, :password, :password_confirmation)
 
-    # Vérification du mot de passe actuel
+    # Vérifie le mot de passe actuel
     if user_params[:current_password].blank? || !current_user.valid_password?(user_params[:current_password])
       return render json: { error: "Mot de passe actuel incorrect ou manquant" }, status: :unauthorized
     end
 
-    # Vérification de la correspondance entre le nouveau mot de passe et la confirmation
-    if user_params[:new_password] != user_params[:new_password_confirmation]
+    # Vérifie que le nouveau mot de passe et la confirmation correspondent
+    if user_params[:password].blank? || user_params[:password] != user_params[:password_confirmation]
       return render json: { error: "La confirmation du mot de passe ne correspond pas" }, status: :unprocessable_entity
     end
 
-    # Mise à jour du mot de passe
-    if current_user.update(password: user_params[:new_password])
+    # Tente de mettre à jour le mot de passe
+    if current_user.update(password: user_params[:password])
       render json: { message: "Mot de passe mis à jour avec succès" }, status: :ok
     else
       render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
